54a9ef4dee5ba37ae40d25d96371f3fb0aa7c0ac
AytacSafarova <aytaccseferr@gmail.com>
2022-09-06 16:54:12 +0400

    uptaded

--- Bubble-game/Bubble-game/main.js
+++ Bubble-game/Bubble-game/main.js
@@ -1,98 +1,107 @@
 
-let startbtn=document.querySelector(".start")
-let savebtn=document.querySelector(".save")
-let stopbtn=document.querySelector(".stop")
-let input= document.querySelector("input")
-let balonplace=document.querySelector(".balonplace")
-let checkboxs=document.querySelectorAll(".checkbox")
-let main =document.querySelector('main') 
- let time=3000;
+let startbtn = document.querySelector(".start")
+let savebtn = document.querySelector(".save")
+let stopbtn = document.querySelector(".stop")
+let input = document.querySelector("input")
+let balonplace = document.querySelector(".balonplace")
+let checkboxs = document.querySelectorAll(".checkbox")
+let main = document.querySelector('main')
+let time = 3000;
 
 
 
 // take count from user
-let count=0
+let count = 0
 let addtime
 let user
 let balons
-let score =0
+let score = 0
 
 startbtn.addEventListener('click', addbubble)
 
 
-  
-  
-checkboxs.forEach(item=>{
-    item.addEventListener('click',()=>{
 
-    if(item.checked && item.nextElementSibling.textContent=="Easy"){
-        clearInterval(addtime)
 
-     addtime= setInterval(()=>{
-            let div= document.createElement("div")
-               div.classList.add("balon")
-               balonplace.appendChild(div) 
-               balons=document.querySelector(".balon")
-               div.addEventListener('click',()=>{
-                div.style.display='none'    
-            score=score+1           })
-         
-           }, time)
-        console.log(item.nextElementSibling.textContent)
-    }
-    else if (item.checked&& item.nextElementSibling.textContent=="Medium"){
-        clearInterval(addtime)
+checkboxs.forEach(item => {
+    item.addEventListener('click', () => {
 
-        time=2000;
-        addtime= setInterval(()=>{
-            let div= document.createElement("div")
-               div.classList.add("balon")
-               balonplace.appendChild(div)
-               div.addEventListener('click',()=>{
-div.style.display='none'       
-score=score+1            })
-           }, time)
-    }
-    else if (item.checked&& item.nextElementSibling.textContent=="Hard"){
-        clearInterval(addtime)
+        if (item.checked && item.nextElementSibling.textContent == "Easy") {
+            clearInterval(addtime)
 
-        time=1000
+            addtime = setInterval(() => {
+                let div = document.createElement("div")
+                div.classList.add("balon")
+                balonplace.appendChild(div)
+                balons = document.querySelector(".balon")
+                div.addEventListener('click', () => {
+                    div.style.display = 'none'
+                    score = score + 1
+                })
 
-        addtime=   setInterval(()=>{
-            let div= document.createElement("div")
-               div.classList.add("balon")
-               balonplace.appendChild(div) 
-               div.addEventListener('click',()=>{
-                div.style.display='none'               })
-           }, time)  
-           score=score+1      }
+            }, 3000)
+            console.log(item.nextElementSibling.textContent)
+        }
+        else if (item.checked && item.nextElementSibling.textContent == "Medium") {
+            clearInterval(addtime)
+
+            time = 2000;
+            addtime = setInterval(() => {
+                let div = document.createElement("div")
+                div.classList.add("balon")
+                balonplace.appendChild(div)
+                div.addEventListener('click', () => {
+                    div.style.display = 'none'
+                    score = score + 1
+                })
+                console.log(score)
+            }, time)
+        }
+        else if (item.checked && item.nextElementSibling.textContent == "Hard") {
+            clearInterval(addtime)
+
+            time = 1000
+
+            addtime = setInterval(() => {
+                let random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+                let div = document.createElement("div")
+                div.classList.add("balon")
+                div.style.left = `${random(0, 430 - 200)}px`
+                div.style.top = `${random(0, 430 - 200)}px`
+
+                //    div.style.top= math.Random()
+                //    div.style.right= math.Random()
+                //    div.style.bottom= math.Random()
+
+                balonplace.appendChild(div)
+                div.addEventListener('click', () => {
+                    div.style.display = 'none'
+                })
+            }, time)
+            score = score + 1
+        }
+
+    })
 
 })
+function addbubble(score) {
+    main.style.display = 'flex'
+    startbtn.style.display = 'none'
+    user = prompt("Please enter your name");
+    let p = document.createElement('p')
+    let text = `${user}, your score is:${score} `
+    p.innerHTML = text
+    main.append(p)
+}
 
-})
-function addbubble(){
-    main.style.display= 'flex' 
-    startbtn.style.display='none'
-  user = prompt("Please enter your name");
-  let p=document.createElement('p')
-  let text= `${user}, your score is:${score} `
-  p.innerHTML=text
-  main.append(p)   }
 
-  
-  
+
 stopbtn.addEventListener('click', stop)
-function stop(){
-clearInterval(addtime)
-console.log('stop')
+function stop() {
+    clearInterval(addtime)
+    console.log('stop')
 }
 savebtn.addEventListener('click', takedata)
-function takedata(){
-    if(input.value){
-        startbtn.disabled=false
-        
-    }
+function takedata() {
+    clearInterval(addtime)
+    localStorage.setItem('username', user);
 }
-// balons.addEventListener('click', ()=>{
-//     console.log('salam')
-// })
